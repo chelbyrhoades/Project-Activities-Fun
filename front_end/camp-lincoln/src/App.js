b0d15a85bde3c46bc89f30c24856b893820c5bdd
Ciara <ciarashaw@ymail.com>
2020-03-31 13:50:28 -0500

    mock pages

--- front_end/camp-lincoln/src/App.js
+++ front_end/camp-lincoln/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from './test.js';
+import Landing from './LandingPage/landing';
 
 function App() {
   return (
-      <Test/>
+      <Landing/>
   );
 }
 
---
+++ front_end/camp-lincoln/src/LandingPage/landing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default class Landing extends Component {
+
+
+
+    render(){
+
+        return(
+            <h1>Lnding Page</h1>
+        );
+
+    }
+}
