9c9f71045b43111cf30eadc689a71be2d61e18fc
Ciara <ciarashaw@ymail.com>
2020-04-10 09:39:27 -0500

    adding stuff

---
+++ front_end/camp-lincoln/src/LoginPage/login.js
@@ -0,0 +1,13 @@
+import { FormControl, Button, FormControlLabel,FormGroup } from '@material-ui/core';
+import React from 'react';
+import { Component } from 'react';
+
+
+export default class Login extends Component{
+
+
+    render(){
+
+        return(<h1>Hello</h1>)
+    }
+}
